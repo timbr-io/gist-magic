e2ea78df34d7db1950de4dfb722de43123ac6c59
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-05-06 11:27:09 -0500

    work towards making it a setuptools package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='gist-magic',
+      version='0.1',
+      description='IPython magics to work with gists',
+      url='https://github.com/pramukta/gist-magic',
+      author='Pramukta Kumar',
+      author_email='pramukta.kumar@timbr.io',
+      license='MIT',
+      packages=['gist_magic', 'gist_magic.extensions'],
+      zip_safe=False,
+      # entry_points = {
+      #   'console_scripts': [
+      #       ]
+      #   },
+      install_requires=[
+          "ipython",
+          "pygithub3"
+        ]
+      )
